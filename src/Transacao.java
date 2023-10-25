8ff2088a62c436a836b5e0fc878016f954891add
Maria-Emilia1 <mariaemiliay99@gmail.com>
2023-10-25 04:42:52 -0300

    desenvolvendo classe transacao

---
+++ src/Transacao.java
@@ -0,0 +1,19 @@
+public class Transacao {
+    private String descricao;
+    private Double valor;
+
+    public Transacao(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+   }   
+
+   public String getDescricao() {
+    return descricao;
+}
+
+public Double getValor() {
+    return valor;
+}
+
+}
+
