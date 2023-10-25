4d0a234bbf86801449cb1574a4104b5f30494d73
Maria-Emilia1 <mariaemiliay99@gmail.com>
2023-10-25 04:43:18 -0300

    desenvolvendo main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] arg) {
+      
+        Cliente cliente = new Cliente("Emilia", "123456789", "01/01/1990", "Rua Principal, 123");
+
+        System.out.println("Nome do cliente: " + cliente.getNome());
+        System.out.println("CPF: " + cliente.getCpf());
+        System.out.println("Data de Nascimento: " + cliente.getDataNascimento());
+        System.out.println("Endereço: " + cliente.getEndereco());
+
+        cliente.setNome("Ludymila");
+        cliente.setDataNascimento("02/02/1985");
+        cliente.setEndereco("Avenida Secundária, 456");
+
+        System.out.println("\nInformações atualizadas do cliente:");
+        System.out.println("Nome do cliente: " + cliente.getNome());
+        System.out.println("Data de Nascimento: " + cliente.getDataNascimento());
+        System.out.println("Endereço: " + cliente.getEndereco());
     }
-}
+ }
